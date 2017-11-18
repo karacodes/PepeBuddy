1ada75d912d3dc3e45c84863cbe4a111dfb5da42
Kara Cavallaro <kara.cavallaro@gmail.com>
2017-11-18 08:32:04 -0800

    Updated workspace and fixed Chirp bug
    API changed for PlaySoundKit and is now PlaySound.

---
+++ out/production/PepeBuddy/README.md
@@ -0,0 +1,2 @@
+# PepeBuddy
+Pepe Buddy - World of Warcraft MOD that puts Pepe the bird on your UI.
---
+++ out/production/PepeBuddy/copy.bat
@@ -0,0 +1,2 @@
+copy /Y D:\Coding\PepeBuddy\PepeBuddy\PepeBuddy.lua "C:\Games\Blizzard\World of Warcraft\interface\addons\PepeBuddy\PepeBuddy.lua"
+copy /Y D:\Coding\PepeBuddy\PepeBuddy\PepeBuddy.toc "C:\Games\Blizzard\World of Warcraft\interface\addons\PepeBuddy\PepeBuddy.toc"
